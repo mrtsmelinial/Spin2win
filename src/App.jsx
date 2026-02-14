f7426b84f01737be2a8579afceb09740a8ab6fac
Timofey Zaichkin <timofejzaickin@gmail.com>
2026-02-14 22:37:07 +0300

    Add history cell

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,30 @@ function createArrayInfo() {
 	return arr
 }
 
+const redNumbers = new Set([
+	1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36,
+])
+
+const rouletteData = Array.from({ length: 37 }, (_, i) => {
+	let color
+
+	if (i === 0) {
+		color = 'g'
+	} else if (redNumbers.has(i)) {
+		color = 'r'
+	} else {
+		color = 'b'
+	}
+
+	return {
+		number: i,
+		color,
+	}
+})
+
 export default function App() {
 	const arrInfo = createArrayInfo()
-	console.log(arrInfo.length)
+
 
 	return (
 		<main className='roulette'>
@@ -49,6 +70,19 @@ export default function App() {
 						</div>
 					</div>
 				</header>
+				<div className='roulette__history'>
+					{rouletteData.slice(10, 20).map((item, index) => (
+						<div
+							className='roulette__history-item'
+							key={index}
+							style={{
+								backgroundColor: `var(--cell-color-${item.color})`,
+							}}
+						>
+							<span className='roulette__history-number'>{item.number}</span>
+						</div>
+					))}
+				</div>
 			</div>
 		</main>
 	)
