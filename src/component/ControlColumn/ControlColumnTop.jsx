6c75a5a89bf068d490f68f13f298f6567f9c7aed
Timofey Zaichkin <timofejzaickin@gmail.com>
2026-02-27 14:32:24 +0300

    Add animation balance

--- src/component/ControlColumn/ControlColumnTop.jsx
+++ src/component/ControlColumn/ControlColumnTop.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import ButtonFullscreen from './Buttons/ButtonFullscreen'
 import ButtonMute from './Buttons/ButtonMute'
 import { useRoulette } from '../../context/RouletteContext'
+import NumberFlow from '@number-flow/react'
 
 export default function ControlColumnTop() {
-	const {state} = useRoulette()
+	const { state } = useRoulette()
 
 	return (
 		<div className='roulette__control-top'>
 			<ButtonFullscreen />
 			<div className='roulette__balance'>
 				<img className='roulette__icon-coin' src='../img/coins.svg' />
-				<span>${state.balance.toFixed(2).replace('.', ',')}</span>
+				<span>
+					$
+					<NumberFlow
+						value={state.balance}
+						format={{
+							minimumFractionDigits: 2,
+							maximumFractionDigits: 2,
+						}}
+					/>
+				</span>
 			</div>
 			<ButtonMute />
 		</div>
