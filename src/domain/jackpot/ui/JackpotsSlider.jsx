e091afaf8d18de07abb59cf1dd9b395ad3b99f30
Timofey Zaichkin <timofejzaickin@gmail.com>
2026-03-22 17:32:00 +0300

    Integration of server data for Jackpots

--- src/domain/jackpot/ui/JackpotsSlider.jsx
+++ src/domain/jackpot/ui/JackpotsSlider.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useRef, useState } from 'react'
 import NumberFlow from '@number-flow/react'
+import { useJackpotStore } from '../model/store'
 
-const slides = [
-	{ title: 'big', src: '/img/jackpot-big.png', amount: 148.1 },
-	{ title: 'super', src: '/img/jackpot-super.png', amount: 1102.05 },
-	{ title: 'mega', src: '/img/jackpot-mega.png', amount: 108600 },
-]
+const JACKPOT_CONFIG = {
+	BIG: { title: 'big', src: '/img/jackpot-big.png' },
+	SUPER: { title: 'super', src: '/img/jackpot-super.png' },
+	MEGA: { title: 'mega', src: '/img/jackpot-mega.png' },
+}
 
 const SLIDE_WIDTH = 214
 const INTERVAL_SLIDE = 5000
 const TRANSITION = 500
 
-const extendedSlides = [...slides, slides[0]]
 
 export default function JackpotSlider() {
+	const jackpots = useJackpotStore(state => state.jackpots)
 	const [currentIndex, setCurrentIndex] = useState(0)
 	const [transition, setTransition] = useState(true)
 	const intervalRef = useRef(null)
+
+	const slides = Object.entries(jackpots).map(([key, data]) => ({
+		title: JACKPOT_CONFIG[key]?.title ?? key.toLowerCase(),
+		src: JACKPOT_CONFIG[key]?.src ?? '',
+		amount: data.current_accumulation,
+		symbol: data.currency_symbol,
+		precision: data.precision,
+	}))
+
+	const extendedSlides = slides.length > 0 ? [...slides, slides[0]] : []
 
 	useEffect(() => {
 		intervalRef.current = setInterval(() => {
@@ -65,10 +76,13 @@ export default function JackpotSlider() {
 				<div className='jackpot-slider__list' style={slideStyle}>
 					{extendedSlides.map((item, index) => (
 						<span key={index} className='jackpot-slider__item'>
-							$
+							{item.symbol}
 							<NumberFlow
 								value={item.amount}
-								format={{ minimumFractionDigits: 2, maximumFractionDigits: 2 }}
+								format={{
+									minimumFractionDigits: item.precision,
+									maximumFractionDigits: item.precision,
+								}}
 							/>
 						</span>
 					))}
